cfbf4d9e4c2cf3767cdd9035745ad687263ff349
John Patrick Fernandez <patzzk.20@gmail.com>
2025-08-07 12:30:18 +0000

    Initial Code Outline

---
+++ script.js
@@ -0,0 +1,42 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // "Order" button
+    var myButton = document.getElementById("myButton");
+    if (myButton) {
+        myButton.addEventListener("click", function() {
+            alert("Welcome!");
+        });
+    }
+
+    // "View Status"
+    var but1 = document.getElementById("but1");
+    if (but1) {
+        but1.addEventListener("click", function() {
+            alert("Cute");
+        });
+    }
+
+    // "Admin Login" button
+    var but2 = document.getElementById("but2");
+    if (but2) {
+        but2.addEventListener("click", function() {
+            window.location.href = "nextpage.html";
+        });
+    }
+
+    // "Back to Home" button
+    var backButton = document.getElementById("backButton");
+    if (backButton) {
+        backButton.addEventListener("click", function() {
+            window.location.href = "index.html";
+        });
+    }
+
+    // "Go to Login Page" button
+    var loginButton = document.getElementById("loginButton");
+    if (loginButton) {
+        loginButton.addEventListener("click", function() {
+            window.location.href = "user.html";
+        });
+    }
+
+});
